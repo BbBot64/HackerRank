0c029e6f8b3204b2046b8352b69c95143a786202
BbBot64 <jortiz16@nyit.edu>
2022-09-03 22:11:20 -0400

    sWAP_cASE Question & Answer

---
+++ Python/sWAP_cASE.py
@@ -0,0 +1,19 @@
+# sWAP cASE
+# 
+# Task
+# 
+# You are given a string and your task is to swap cases.
+# In other words, convert all lowercase letters to uppercase letters and vice versa.
+def swap_case(s):
+    result = ''
+    for i in s:
+        if i == i.lower():
+            result += i.upper()
+        elif i == i.upper():
+            result += i.lower()
+    return result
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
